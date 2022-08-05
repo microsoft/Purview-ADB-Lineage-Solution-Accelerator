565d23a948e17b901d61dd7d78e172785c295072
TravisHilbert <59578919+TravisHilbert@users.noreply.github.com>
2022-08-05 15:28:37 -0700

    Update ColParser.cs

--- function-app/adb-to-purview/src/Function.Domain/Helpers/parser/ColParser.cs
+++ function-app/adb-to-purview/src/Function.Domain/Helpers/parser/ColParser.cs
@@ -40,45 +40,53 @@ namespace Function.Domain.Helpers
         {
             
             var col = new List<ColumnLevelAttributes>();
-            var dataSetList = new DatasetMappingClass();
-            var columnLevelList = new List<ColumnMappingClass>();
-            var dataSet = new DatasetMappingClass();
-            var cla = new ColumnLevelAttributes();
             foreach(Outputs colId in _olEvent.Outputs)
             {   
                 foreach(KeyValuePair<string, ColumnLineageInputFieldClass> colInfo in colId.Facets.ColFacets.fields)
                 {
-                    
-                    //Set sink name 
-                    dataSet.sink = _qnParser.GetIdentifiers(colId.NameSpace, colId.Name).QualifiedName;
-                    var columnLevel = new ColumnMappingClass();
-                    foreach(ColumnLineageIdentifierClass colInfo2 in colInfo.Value.inputFields)
+                  var dataSet = new DatasetMappingClass();
+                    dataSet.sink = $"{colId.NameSpace}, {colId.Name}";
+                    var columnLevels = new List<ColumnMappingClass>();
+                    foreach (ColumnLineageIdentifierClass colInfo2 in colInfo.Value.inputFields)
                     {
+                        var columnLevel = new ColumnMappingClass();
                         //get sources for column level list 
-                        dataSet.source = _qnParser.GetIdentifiers(colInfo2.nameSpace, colInfo2.name).QualifiedName;
+                        dataSet.source = $"{colInfo2.nameSpace}, {colInfo2.name}";
                         columnLevel.source = colInfo2.field;
                         columnLevel.sink = colInfo.Key;
-                        //ADD data to model
-                        //dataSetList.source.GroupBy(x=>x.Equals(dataSet.source));
-                        // dataSetList.Add(dataSet);
-                        //dataSetList = dataSet;
-                        //columnLevelList.Add(columnLevel);
-                       
+                        columnLevels.Add(columnLevel);
                     }
-                    //Add data to list 
-                    cla.datasetMapping = dataSet;
-                    cla.columnMapping.Add(columnLevel);
-                    col.Add(cla);
+                    
+                    var cla = new ColumnLevelAttributes(); 
+                    if( col.Count == 0)
+                    {
+                        cla.datasetMapping = dataSet;
+                        cla.columnMapping = columnLevels;
+                    }
+                    foreach(var ob in col)
+                    {
+                        if(ob.datasetMapping.source == dataSet.source && col.Count >= 1)
+                        {
+                            var cla2 = new ColumnLevelAttributes(); 
+                            cla2.columnMapping = columnLevels;
+                            ob.columnMapping.Add(cla2.columnMapping[0]);
+                            break;
+                        }
+                        else
+                        {
+                            cla.datasetMapping = dataSet;
+                            cla.columnMapping = columnLevels;
+                            break;
+                        }
+                    }
+                    //Add data to list
+                    if(cla.datasetMapping.source != string.Empty && cla.datasetMapping.sink != string.Empty)
+                    {
+                        col.Add(cla);
+                    }
                 }
             }
-            // var test = col.datasetMapping.source.GroupBy(x=>x.Equals(col.datasetMapping.sink));
-            // //var test = col.datasetMapping.source.GroupBy(x);
-            //var test2 = col.GroupBy(x=>x.datasetMapping.sink);
-
-            //string jsonString = JsonSerializer.Serialize(col);
             string jsonString2 = JsonConvert.SerializeObject(col);
-           // string jsonString3 = JsonConvert.SerializeObject(test);
-           // string jsonString = JsonConvert.SerializeObject(test2);
             return col; 
         }
     }
