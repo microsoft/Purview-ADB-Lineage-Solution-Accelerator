a8e6a3982cbe855026983dbc9794386c44d35373
Will Johnson <will@willj.co>
2023-03-12 22:51:16 -0500

    Correct one line mappings as artifact

--- deployment/util/mappings-remove-spaces.py
+++ deployment/util/mappings-remove-spaces.py
@@ -1,15 +1,14 @@
 import argparse
+import os
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("mappings_json", help="File path of the mappings json")
-    parser.add_argument("output_path", help="File path where the oneline json file should land")
     args, unknown_args = parser.parse_known_args()
 
     with open(args.mappings_json, 'r') as fp:
         mappings = fp.read()
 
     oneliner = mappings.replace("\n", "").replace(" ", "")
-    with open(args.output_path, 'w') as output:
-        output.write(oneliner)
+    print(oneliner)
