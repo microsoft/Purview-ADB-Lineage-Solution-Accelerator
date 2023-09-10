b60232c64ae200493b25e8a6f74e08a84f315064
Will Johnson <will@willj.co>
2023-09-10 10:12:46 -0500

    Update Azure MySQL mapping to reflect OpenLineage1.1.0 name containing database

--- function-app/adb-to-purview/tests/unit-tests/Function.Domain/Helpers/Parser/QnParserTests.cs
+++ function-app/adb-to-purview/tests/unit-tests/Function.Domain/Helpers/Parser/QnParserTests.cs
@@ -125,9 +125,9 @@ namespace UnitTests.Function.Domain.Helpers
                     "sales.region", 
                     "mssql://purviewadbsynapsews.sql.azuresynapse.net/SQLPool1/sales/region")]
         // Azure MySQL
-        [InlineData("mysql://fikz4nmpfka4s.mysql.database.azure.com:3306/mydatabase", 
-                    "fruits", 
-                    "mysql://fikz4nmpfka4s.mysql.database.azure.com/mydatabase/fruits")]
+        [InlineData("mysql://sample.mysql.database.azure.com:3306", 
+                    "mydatabase.fruits", 
+                    "mysql://sample.mysql.database.azure.com/mydatabase/fruits")]
         // Azure Postgres Public
         [InlineData("postgresql://gqhfuzgnrmpzw.postgres.database.azure.com:5432/postgres", 
             "people", 
