4eba25b5c409def0f35ea90dcafe8b6c84506f7f
Will Johnson <will@willj.co>
2023-12-23 20:34:31 -0600

    Hotfix/max query plan ol in (#226)
    * Simplify reducing the payload size by introducing a maxPayloadSize and removing maxQueryPlanSize. Now we will remove spark plan first, then column lineage
    
    ---------
    
    Co-authored-by: Hanna Moazam <hannamoazam@gmail.com>

--- function-app/adb-to-purview/src/Function.Domain/Services/OlFilter.cs
+++ function-app/adb-to-purview/src/Function.Domain/Services/OlFilter.cs
@@ -33,24 +33,19 @@ namespace Function.Domain.Services
         /// <summary>
         /// Filters the OL events to only those that need to be passed on to the event hub
         /// </summary>
-        /// <param name="strRequest">This is the OpenLineage data passed from the Spark listener</param>
+        /// <param name="olEvent">This is the OpenLineage data passed from the Spark listener</param>
         /// <returns> 
         /// true: if the message should be passed on for further processing
         /// false: if the message should be filtered out
         /// </returns>
-        public bool FilterOlMessage(string strRequest)
+        public bool FilterOlMessage(Event olEvent)
         {
-            var olEvent = ParseOlEvent(strRequest);
             try {
-                if (olEvent is not null)
-                {
-                    var validateEvent = new ValidateOlEvent(_loggerFactory);
-                    return validateEvent.Validate(olEvent);
-                }
-                return false;
+                var validateEvent = new ValidateOlEvent(_loggerFactory);
+                return validateEvent.Validate(olEvent);
             }
             catch (Exception ex) {
-                _logger.LogError(ex, $"Error during event validation: {strRequest}, error: {ex.Message}");
+                _logger.LogError(ex, $"Error during event validation: {olEvent.ToString()}, error: {ex.Message}");
                 return false;
             }
         }
@@ -60,37 +55,9 @@ namespace Function.Domain.Services
         /// </summary>
         /// <param name="strRequest"> the OpenLineage event </param>
         /// <returns> the namespace value from the event </returns>
-        public string GetJobNamespace(string strRequest)
+        public string GetJobNamespace(Event olEvent)
         {
-            var olEvent = ParseOlEvent(strRequest);
             return olEvent?.Job.Namespace ?? "";
-        }
-
-        private Event? ParseOlEvent(string strEvent)
-        {
-            try{
-                var trimString = TrimPrefix(strEvent);
-                var _event = JsonConvert.DeserializeObject<Event>(trimString);
-                int planSize = System.Text.Encoding.Unicode.GetByteCount(_event!.Run.Facets.SparkLogicalPlan.ToString());
-                if (planSize > _appSettingsConfig!.maxQueryPlanSize){
-                    _logger.LogWarning("Query Plan size exceeded maximum. Removing query plan from OpenLineage Event");
-                    _event.Run.Facets.SparkLogicalPlan = new JObject();
-                }
-                return _event;
-            }
-            catch (JsonSerializationException ex) {
-                _logger.LogWarning($"Json Serialization Issue: {strEvent}, error: {ex.Message} path: {ex.Path}");
-            }
-            // Parsing error
-            catch (Exception ex){
-                _logger.LogWarning($"Unrecognized Message: {strEvent}, error: {ex.Message}");
-            }
-            return null;
-        }
-        
-        private string TrimPrefix(string strEvent)
-        {
-            return strEvent.Substring(strEvent.IndexOf('{')).Trim();
         }
     }
 }
